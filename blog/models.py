8947920e2bbe6b0b37c72fa0c25371a6ce06f858
David Fee <davidtfee@yahoo.co.uk>
2021-08-13 09:56:36 +0100

    Updating model.

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=50)
+    text = models.TextField(max_length=300)
+    created_timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.text}\n[{author}, {created_timestamp}]"
